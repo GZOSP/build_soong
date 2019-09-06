b0a1c85afe1323f871b3c65be8ba78d93254433f
Prema Chand Alugu <premaca@gmail.com>
2019-09-06 21:31:21 +0530

    [TEMP] Revert SetupPath: to allow aarch64-linux-android-gcc

--- ui/build/build.go
+++ ui/build/build.go
@@ -154,8 +154,6 @@ func Build(ctx Context, config Config, what int) {
 
 	ensureEmptyDirectoriesExist(ctx, config.TempDir())
 
-	SetupPath(ctx, config)
-
 	if config.StartGoma() {
 		// Ensure start Goma compiler_proxy
 		startGoma(ctx, config)
